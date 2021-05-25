42b0070b2fc61299b32bc3f7d70472eb41caffe1
MG <marcosg1996@hotmail.com.ar>
2021-05-25 17:11:37 -0300

    25/5

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import React from "react"
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+            <h1></h1>
+            <form>
+                <input placeholder="Nombres" name="Nombres" type="text" className=""/>
+                <input placeholder="Apellidos" name="Apellidos" type="text" className=""/>
+                <input placeholder="Celular" name="Celular" type="num" className=""/>
+                <textarea placeholder="Sobre Mi" name="SobreMi" type="text" className=""/>
+            </form>
+        </div>
   );
 }
 
